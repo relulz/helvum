e1fbb0cf498b8a3e245dda6fd34c986ab03031e1
Mihai Fufezan <fufexan@pm.me>
2021-12-18 19:50:39 +0000

    Use gtk4-update-icon-cache instead of gtk3 one

--- build-aux/meson_post_install.py
+++ build-aux/meson_post_install.py
@@ -7,6 +7,6 @@ if not environ.get('DESTDIR', ''):
     PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
     DATA_DIR = path.join(PREFIX, 'share')
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
+    call(['gtk4-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
     print("Updating desktop database...")
     call(["update-desktop-database", path.join(DATA_DIR, 'applications')])
